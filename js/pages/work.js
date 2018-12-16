d5bc023e61a436bcec613eaff7d2a636db3e7b37
leochocolat <l.mouraire@outlook.fr>
2018-12-16 15:44:19 +0100

    Fonctions preview images projects

--- js/pages/work.js
+++ js/pages/work.js
@@ -3,4 +3,14 @@ $(".project-item a").hover(function(){
   $(".project-item a").not($(this)).removeClass("active");
   $(this).addClass("active");
 
+  let tabClass = []
+  tabClass = $(this)[0].className.split(/\s+/);
+  let classImg = tabClass[0] + "-img";
+
+  $(".project-apercu").not($(classImg)).removeClass("active");
+  $("." + classImg).addClass("active");
+
+
+
+
 });
